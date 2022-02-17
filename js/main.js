27d2a32275a4317d510668c438bd335795521134
hasifazhaan <hasif45azhaan@gmail.com>
2022-02-17 23:14:13 +0530

    added event page

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+          $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('navbar-scroll');
+    }
+
+    else {
+          $('nav').removeClass('navbar-scroll');
+    }
+})
