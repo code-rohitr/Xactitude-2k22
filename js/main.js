0442d6367f85c316b4375b34a1701f5f55d4daba
hasifazhaan <hasif45azhaan@gmail.com>
2022-02-17 23:14:48 +0530

    added event page

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
     });
 });
 
-// Scrolling Effect
-
 $(window).on("scroll", function() {
     if($(window).scrollTop()) {
           $('nav').addClass('navbar-scroll');
