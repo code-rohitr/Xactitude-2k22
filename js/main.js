f14ef7d486cde4e3ffa5abaf2483281f180a13cf
hasifazhaan <hasif45azhaan@gmail.com>
2022-02-21 15:30:43 +0530

    Bug Solved in contact responsive

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 $(window).on("scroll", function () {
   if ($(window).scrollTop()) {
     $(".nav").addClass("navbar-scroll");
+   
   } else {
     $(".nav").removeClass("navbar-scroll");
   }
